b103673e8cfcf09b0168cd37bae8ac9cce6ffc39
Melvin He <melvin_he@brown.edu>
2022-12-30 18:11:55 -0500

    projects setup

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import ProjectBlock from './ProjectBlock';
+import projects from '../data/projects.json';
+import Section from './Section';
 
 export default function Projects() {
     return (
         <div>
             <NavBar />
-            <h1>Projects</h1>
+            {/*Projects section*/}
+            <Section title="Projects">
+                <ProjectBlock projects={projects}/>
+            </Section>
         </div>
     );
 }
